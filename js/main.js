a16151b646270bff41998cebab55b331811e5c9e
pedro <pedro@lar21.com>
2016-07-30 17:52:35 -0300

    Adding pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+var pubnub = PUBNUB.init({
+  publish_key: 'demo',
+  subscribe_key: 'demo'
 
+
+var channel = "c3-spline" + Math.random();
+eon.chart({
+  channel: channel,
+  history: true,
+  flow: true,
+  pubnub: pubnub,
+  generate: {
+    bindto: '#charteon',
+    data: {
+      labels: false
+    }
+  }
+});
