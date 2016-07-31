058db70049fe5f08c69f2d285e03b8940e9e7f15
pedro <pedro@lar21.com>
2016-07-31 00:25:18 -0300

    Adding pages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var pubnub = PUBNUB.init({
   publish_key: 'pub-c-288b24ab-5534-4fc7-9f7e-3f18a963664a',
-  subscribe_key: 'sub-c-7770a3e2-5699-11e6-b1c5-0619f8945a4f'
+  subscribe_key: 'sub-c-7770a3e2-5699-11e6-b1c5-0619f8945a4f',
+  ssl : (('https:' == document.location.protocol) ? true : false)
 });
 
 eon.chart({
